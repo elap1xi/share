771821edd28b3f7ae6c2b73c05e81d3bbdcc0c6f
elap1_xi <gangsangwon40@gmail.com>
2024-07-03 09:57:30 +0900

    THISISTESTCOMMIT

---
+++ main.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.post('/webhook', (req, res) => {
+    console.log(req.body);
+});
+
+app.listen(8000, () => {
+  console.log('Server listening on port 8000');
+});
