029528a2e5d7cd7deb8038d91fa3d59ec2a91d7a
elap1_xi <gangsangwon40@gmail.com>
2024-07-03 10:20:18 +0900

    Test_20240703

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ app.post('/webhook', (req, res) => {
         console.log(req.body);
         res.status(200).send('ok.');
     } else {
-        console.log(req.body);
+        let body = req.body;
+        console.log(`Pusher : ${body.pusher.name}\nCommit MSG : ${body.commits[0].message} | ${body.commits[0].timestamp}`);
         res.status(200).send('ok');
     }
 });
